e7966d75cef2d5b66662de25bc35877e8a6808ba
Andre Heijstek <andre.heijstek@hey.com>
2022-04-06 18:50:43 +0200

    Dry up examples

--- spec/system/route_goal_spec.rb
+++ spec/system/route_goal_spec.rb
@@ -1,62 +1,43 @@
 # frozen_string_literal: true
 
+# TODO: Naming: route or trip?
+
 describe 'route_goals', type: :system do
-  it 'creates correctly' do
-    NewRouteGoalPage.new.tap do |n|
+  let(:page) { NewRouteGoalPage.new }
+  before do
+    page.tap do |n|
       n.load
       n.from_field.set(Faker::Address.city)
       n.to_field.set(Faker::Address.city)
       n.departure_radio_button.choose(allow_label_click: true)
       n.date_field.set("01-01-2022")
       n.moment_field.set("avond")
-      n.create_route_goal_button.click
     end
+  end
+
+
+  it 'creates a typical route correctly' do
+    page.load
+    page.create_route_goal_button.click
 
     expect(page).to have_content("Route goal was successfully created")
   end
 
   it 'selects aankomst correctly' do
-    NewRouteGoalPage.new.tap do |n|
-      n.load
-      n.from_field.set(Faker::Address.city)
-      n.to_field.set(Faker::Address.city)
-      n.arrival_radio_button.choose(allow_label_click: true)
-      n.date_field.set('01-01-2022')
-      n.moment_field.set('avond')
-      n.create_route_goal_button.click
-    end
+    page.load
+    page.arrival_radio_button.choose(allow_label_click: true)
+    page.create_route_goal_button.click
 
     expect(page).to have_content('arrival')
     expect(page).to_not have_content('departure')
   end
 
   it 'selects vertrek correctly' do
-    NewRouteGoalPage.new.tap do |n|
-      n.load
-      n.from_field.set(Faker::Address.city)
-      n.to_field.set(Faker::Address.city)
-      n.departure_radio_button.choose(allow_label_click: true)
-      n.date_field.set('01-01-2022')
-      n.moment_field.set('avond')
-      n.create_route_goal_button.click
-    end
+    page.load
+    page.departure_radio_button.choose(allow_label_click: true)
+    page.create_route_goal_button.click
 
     expect(page).to have_content('departure')
     expect(page).to_not have_content('arrival')
-  end
-
-  it 'selects aankomst/vertrek correctly' do
-    NewRouteGoalPage.new.tap do |n|
-      n.load
-      n.from_field.set(Faker::Address.city)
-      n.to_field.set(Faker::Address.city)
-      n.arrival_radio_button.choose(allow_label_click: true)
-      n.date_field.set('01-01-2022')
-      n.moment_field.set('avond')
-      n.create_route_goal_button.click
-    end
-
-    expect(page).to have_content('arrival')
-    expect(page).to_not have_content('departure')
   end
 end
