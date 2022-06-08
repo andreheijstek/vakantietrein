ed1095c71e285fab97035e13942a8f67ba656424
Andre Heijstek <andre.heijstek@hey.com>
2022-06-08 23:30:30 +0200

    First test on the box

--- spec/system/route_goal_train_box.rb
+++ spec/system/route_goal_train_box.rb
@@ -2,23 +2,19 @@
 
 describe 'route_goals', type: :system do
   let(:page) { NewRouteGoalPage.new }
-  # before do
-  #   @from_city = Faker::Address.city
-  #   @to_city = Faker::Address.city
-  #   page.load
-  #   page.from_field.set(@from_city)
-  #   page.to_field.set(@to_city)
-  # end
-
-  it 'shows the From city' do
-    @from_city = Faker::Address.city
-    @to_city = Faker::Address.city
+  let(:from_city) { Faker::Address.city }
+  let(:to_city) {Faker::Address.city }
+  before do
     page.load
     page.from_field.set(@from_city)
     page.to_field.set(@to_city)
+  end
+
+  it 'shows the box' do
+    page.load
     page.create_route_goal_button.click
-    save_and_open_page
-    expect(page).to have_content(@from_city)
+    box = find(:id, "main_box")
+    expect(box.tag_name).to eq('rect')
   end
 end
 
